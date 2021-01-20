cddf9b12e1b7e908e1d12576c29db209633c2ef0
gwarz <gmwarzecha@tutanota.com>
2021-01-19 20:02:27 -0700

    connected both pages' functionality

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,12 +1,29 @@
 
 var issuesContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function (repo) {
-  console.log(repo);
-
+  // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+  // make a request to url
   fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
@@ -18,12 +35,14 @@ var getRepoIssues = function (repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-      alert("There was a problem with your request!");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
+
+
 
 var displayIssues = function(issues) {
   if (issues.length === 0) {
@@ -66,6 +85,7 @@ var displayWarning = function(repo) {
   // add text to warning container
   limitWarningEl.textContent = "To see more than 30 issues, visit ";
   
+  // create link element
   var linkEl = document.createElement("a");
   linkEl.textContent = "See More Issues on GitHub.com";
   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
@@ -76,5 +96,4 @@ var displayWarning = function(repo) {
 };
 
 
-
-getRepoIssues("angular/angular");
+getRepoName();
